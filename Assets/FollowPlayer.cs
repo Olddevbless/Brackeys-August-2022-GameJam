6eb8d3e62cb63a1ff896902d01fdabdd4a29dcaf
Olddevbless <Olddevbless@users.noreply.github.com>
2022-08-21 18:26:46 +0300

    dog AI and basic movements

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] float followDelay;
+    [SerializeField] float followDistance;
+    [SerializeField] bool isFollowing;
+    [SerializeField] bool playerIsMoving;
+    NavMeshAgent nav;
+
+    void Start()
+    {
+        
+       
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerIsMoving = target.GetComponent<PlayerMovement>().playerIsMoving;
+        if (isFollowing == true && playerIsMoving == true)
+        {
+            Invoke("Follow", 1f);
+        }
+        if (Mathf.Abs(target.transform.position.x - this.transform.position.x) > followDistance)
+        {
+            isFollowing = true;
+        }
+        if (Mathf.Abs(target.transform.position.x - this.transform.position.x) < followDistance)
+        {
+            isFollowing = false;
+        }
+       
+    }
+    void Follow()
+    {
+        nav.destination = new Vector3(target.transform.position.x-followDistance,target.transform.position.y,target.transform.position.z);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody playerRB;
+    [SerializeField] float speed = 1;
+    [SerializeField] float horizontal;
+    [SerializeField] float jumpForce= 10;
+    [SerializeField] bool isGrounded;
+    [SerializeField] public bool playerIsMoving;
+    
+
+    private void Awake()
+    {
+        playerRB = this.GetComponent<Rigidbody>();
+           
+
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        if (playerRB.velocity != Vector3.zero)
+        {
+            playerIsMoving = true;
+        }
+        else
+        {
+            playerIsMoving = false;
+        }
+        Movement();
+        Jump();
+    }
+    public void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded==true)
+        {
+            playerRB.AddForce(Vector3.up *jumpForce, ForceMode.Impulse);
+        }
+    }
+    void Movement()
+    {
+      
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+            //playerRB.AddForce(new Vector3(horizontal, 0, 0)*speed, ForceMode.Force);
+            this.transform.Translate(new Vector3(horizontal,0,0) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+            //playerRB.AddForce(new Vector3(horizontal, 0, 0) * speed, ForceMode.Force);
+            this.transform.Translate(new Vector3(horizontal,0,0)  * speed * Time.deltaTime);
+        }
+        
+        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
